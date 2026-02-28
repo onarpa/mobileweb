4a3bdcc7204f73839d61122a7685b1f384882bb4
onarpa <onarpa.l@kkumail.com>
2026-02-28 16:13:27 +0700

    add lab09_sensors

---
+++ docs/lab09-sensors/dist/assets/web-legacy-Bdlh_Xtw.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-DVhHv7UK.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.W}],execute:function(){e("MotionWeb",class extends i{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}})}}});
